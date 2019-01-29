c8b77990de75a382c21ee3f40e94b40337570bb8
LongYinan <lynweklm@gmail.com>
2019-01-29 19:03:46 +0800

    ci: add circular check and project builds

---
+++ packages/http/src/index.ts
@@ -0,0 +1,2 @@
+export * from './http-client'
+export { Interceptor } from './interceptor'
---
+++ rollup.config.js
@@ -0,0 +1,41 @@
+export default [
+  {
+    input: './packages/http/esm/index.js',
+    output: [
+      {
+        file: './packages/http/lib/index.js',
+        format: 'cjs'
+      },
+      {
+        file: './packages/http/esm/bundle.esm.js',
+        format: 'esm'
+      }
+    ]
+  },
+  {
+    input: './packages/utils/esm/index.js',
+    output: [
+      {
+        file: './packages/utils/lib/index.js',
+        format: 'cjs'
+      },
+      {
+        file: './packages/utils/esm/bundle.esm.js',
+        format: 'esm'
+      }
+    ]
+  },
+  {
+    input: './packages/types/esm/index.js',
+    output: [
+      {
+        file: './packages/types/lib/index.js',
+        format: 'cjs'
+      },
+      {
+        file: './packages/types/esm/bundle.esm.js',
+        format: 'esm'
+      }
+    ]
+  }
+]
