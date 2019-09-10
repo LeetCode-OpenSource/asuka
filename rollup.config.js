ab3db08e3692748b2220cd39e508824a9f5b9d27
LongYinan <lynweklm@gmail.com>
2019-09-10 11:14:26 +0800

    style: lint issues in rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -4,51 +4,51 @@ export default [
     output: [
       {
         file: './packages/http/lib/index.js',
-        format: 'cjs'
+        format: 'cjs',
       },
       {
         file: './packages/http/esm/bundle.esm.js',
-        format: 'esm'
-      }
-    ]
+        format: 'esm',
+      },
+    ],
   },
   {
     input: './packages/di/esm/index.js',
     output: [
       {
         file: './packages/di/lib/index.js',
-        format: 'cjs'
+        format: 'cjs',
       },
       {
         file: './packages/di/esm/bundle.esm.js',
-        format: 'esm'
-      }
-    ]
+        format: 'esm',
+      },
+    ],
   },
   {
     input: './packages/utils/esm/index.js',
     output: [
       {
         file: './packages/utils/lib/index.js',
-        format: 'cjs'
+        format: 'cjs',
       },
       {
         file: './packages/utils/esm/bundle.esm.js',
-        format: 'esm'
-      }
-    ]
+        format: 'esm',
+      },
+    ],
   },
   {
     input: './packages/types/esm/index.js',
     output: [
       {
         file: './packages/types/lib/index.js',
-        format: 'cjs'
+        format: 'cjs',
       },
       {
         file: './packages/types/esm/bundle.esm.js',
-        format: 'esm'
-      }
-    ]
-  }
+        format: 'esm',
+      },
+    ],
+  },
 ]
